6ddebf6e5a729b54127590a045ede363bf5ce5e6
lili <lili@gmail.com>
2023-08-08 12:23:03 +0000

    commit tout la partie1

---
+++ partie1/index.js
@@ -0,0 +1,123 @@
+
+
+/* let nom;// declaration de la variable 
+nom = "Laeticia";// affectation de la variable 
+//camelCase
+let nomEtudiant = "Reddington";
+const nombreDevie = 100;
+
+console.log(nom,nomEtudiant,nombreDevie);
+
+
+let age = prompt("quelle est votre age?");
+console.log(age); */
+
+//let age = prompt("quelle est votre age");
+//console.log(age);
+
+/* let nomEwe = prompt("quelle est votre nom ewe  ");
+let Prenom = prompt("quelle est votre prenom ");
+
+console.log("bienvenue" + Prenom); */
+
+//condition
+
+/* let age = 17;
+if(age <= 17){
+    console.log("c'est une mineur");
+}else{
+    console.log("tu peux aller");
+}
+
+let nom = "lili"
+let prenom = "lili"
+
+if(nom == prenom){
+    console.log("c'est bizare");
+}else{
+    console.log("ca va ");
+}
+
+let number = Number( prompt("veuiler choisir un nombre") ); */
+/* console.log(number + 1);
+console.log(typeof number); */
+
+
+
+/* if(number>0){
+    console.log("positif");
+}else if (number == 0) {
+    console.log("nul");
+}else{
+    console.log("negative");
+}
+    
+
+let sexe = prompt("Quelle est votre sexe, f pour feminin, m pour masculin");
+let fesse = prompt("est ce que tu as fesse ? o pour oui et n pour non");
+
+if(sexe == " f" && fesse == "o"){
+    console.log("Ton anevir est collé derriere toi ");
+}else{
+    console.log("Travail dur, tu dois manger a la sueur de ton front");
+}
+
+if(sexe == "m" || fesse == "n"){
+    console.log("je t'es dit de te cocentrer sur le travail");
+} */
+
+/* let lundi = "kodjo";
+let mardi = "ablam";
+let mercredi = "kokou";
+let jeudi = "yao";
+let vendredi = "afi";
+let samedi = "komi";
+let dimanche = "kossi";
+
+
+
+
+switch("afi"){
+    case "kodjo":
+        console.log("vous êtes né un lundi");
+        break;
+        case "abla":
+            console.log("vous êtes né un mardi");
+            break;
+            case "kokou":
+                console.log("vous êtes né un mercrdi");
+                break;
+                case "yao":
+                    console.log("vous êtes né un jeudi");
+                    break;
+                    case "afi":
+                        console.log("vous êtes né un vendredi");
+                        break;
+                        case "komi":
+                            console.log("vous êtes né un samedi");
+                            break;
+                            case "kossi":
+                                console.log("vous êtes né un dimanche");
+                                break;
+
+        default:
+            console.log("votre nom doit être erronné");
+            break;
+}
+ */
+
+
+//boucle while & for
+
+ let specialite = prompt("Quelle specialité faite vous ? ")
+while(specialite != "IT"){
+    console.log("tu n'a pas ta place ici");
+    specialite = prompt("quelle specialite faite vous ?")
+
+} 
+
+/* let nbr = 1;
+while(nbr < 6){
+    console.log("inferieur");
+    nbr ++; 
+} */
