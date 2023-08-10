3f42e9e68d3db2b56471ab2b9c1a0afc01dc87f1
lili <lili@gmail.com>
2023-08-10 12:32:47 +0000

    commit les mises à jour

--- partie3/index.js
+++ partie3/index.js
@@ -8,19 +8,25 @@ class Personnage{
     }
     experience = 0;
     niveauDeVie =100;
-    arme = [{nom:"fusil",dega:100},
+    arme = [{nom:"fusil",dega:35},
             {nom:"couteau",dega:10},
-            {nom:"machette",dega:500}
+            {nom:"machette",dega:20}
             ]
 
     presentation(){
         console.log(`je m'appelle ${this.nom} `)
     }
-}
-let arme = (parametre) =>{
-    for(let objet of parametre){
-        console.log(`afficher les armes ${objet.nom},${objet.dega}`);
+    Attaque(Personnage,arme){
+        if(arme == "fusil"){
+            Personnage.niveauDeVie = Personnage.niveauDeVie - 35
+            console.log(`${this.nom} attaque avec ${arme} ${Personnage.nom} a qui il reste ${Personnage.niveauDeVie - 35} `);
+        }
+        
     }
+
+
 }
+let personne1 = new Personnage("lili");
+let personne2 = new Personnage("grace");
 
-
+personne1.Attaque(personne2,"fusil");
