dcc123483c39550b7c8f4e51c1e6ca05791c1ea3
lili <lili@gmail.com>
2023-08-10 14:43:41 +0000

    commit les mises à jours

--- partie3/index.js
+++ partie3/index.js
@@ -30,3 +30,35 @@ let personne1 = new Personnage("lili");
 let personne2 = new Personnage("grace");
 
 personne1.Attaque(personne2,"fusil");
+
+
+let utilisateur = prompt("Entrer votre nom");
+
+class Acteur{
+    constructor(nom,prenom,age){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+start = true;
+}
+class Etudiant extends Acteur{
+    constructor(nom,prenom,age,filière){
+        super(nom,prenom,age);
+        this.filière = filière;
+    }
+    note = [];
+
+    calulMoyenne(){
+        if(this.note.length< 1){
+            console.log("Tu ne peut pas calculer la moyenne");
+        }else {
+            console.log("La moyenne est calculabre");
+        }
+
+    }
+
+
+}
+let afficher = new Etudiant("akossiwa","juvincia",18,"D");
+console.log(afficher);
